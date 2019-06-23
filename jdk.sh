a6ec68a265f9d73f9d3dfee3c26452d2736ba887
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2019-06-23 20:17:44 +0700

    Update jdk.sh

--- jdk.sh
+++ jdk.sh
@@ -1,12 +1,24 @@
 tar -xvf jdk-8u211-linux-arm64-vfp-hflt.tar.gz
-sudo mkdir -p /usr/lib/jvm
-sudo mv ./jdk1.8.0_211 /usr/lib/jvm/
+mkdir -p /usr/lib/jvm
+mv ./jdk1.8.0_211 /usr/lib/jvm/
 
-sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0_211/bin/java" 1
-sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.8.0_211/bin/javac" 1
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.8.0_211/bin/javaws" 1
+update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0_211/bin/java" 1
+update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.8.0_211/bin/javac" 1
+update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.8.0_211/bin/javaws" 1
 
-sudo chmod a+x /usr/bin/java
-sudo chmod a+x /usr/bin/javac
-sudo chmod a+x /usr/bin/javaws
-sudo chown -R root:root /usr/lib/jvm/jdk1.8.0_211
+chmod a+x /usr/bin/java
+chmod a+x /usr/bin/javac
+chmod a+x /usr/bin/javaws
+chown -R root:root /usr/lib/jvm/jdk1.8.0_211
+
+cd ..
+
+wget https://github.com/gama-platform/gama.cloud/releases/download/latest/GamaWeb.zip
+
+apt install unzip
+
+unzip GamaWeb.zip
+mv GamaWeb/GamaWeb.war GamaWeb/offline_GamaWeb.war
+chmod -R +777 GamaWeb
+cd GamaWeb
+java -jar tomcat_launcher.jar "/offline_GamaWeb" "offline_GamaWeb.war"
