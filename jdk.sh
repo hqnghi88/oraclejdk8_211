12e118c711f4b77a69a727e8c37f2961804cf575
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2019-06-23 20:43:39 +0700

    Update jdk.sh

--- jdk.sh
+++ jdk.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+extract_jdk(){
 tar -xvf jdk-8u211-linux-arm64-vfp-hflt.tar.gz
 mkdir -p /usr/lib/jvm
 mv ./jdk1.8.0_211 /usr/lib/jvm/
@@ -13,6 +15,10 @@ chown -R root:root /usr/lib/jvm/jdk1.8.0_211
 
 cd ..
 
+}
+
+install_gamaweb(){
+
 wget https://github.com/gama-platform/gama.cloud/releases/download/latest/GamaWeb.zip
 
 apt install unzip
@@ -22,3 +28,11 @@ mv GamaWeb/GamaWeb.war GamaWeb/offline_GamaWeb.war
 chmod -R +777 GamaWeb
 cd GamaWeb
 java -jar tomcat_launcher.jar "/offline_GamaWeb" "offline_GamaWeb.war"
+}
+if [ -e ${/usr/lib/jvm/jdk1.8.0_211} ]
+then
+install_gamaweb
+else
+extract_jdk
+install_gamaweb
+fi
