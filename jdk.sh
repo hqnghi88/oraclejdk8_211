3a6fdbdf02fd76e17fdab1b1b2b3d50d28fe344b
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2019-06-24 19:52:22 +0700

    Update jdk.sh

--- jdk.sh
+++ jdk.sh
@@ -27,11 +27,11 @@ wget https://github.com/gama-platform/gama.cloud/releases/download/latest/GamaWe
 unzip GamaWeb.zip
 mv GamaWeb/GamaWeb.war GamaWeb/offline_GamaWeb.war
 cd GamaWeb
-java -jar tomcat_launcher.jar /offline_GamaWeb offline_GamaWeb.war 8080 extract
+java -jar tomcat_launcher.jar /offline_GamaWeb offline_GamaWeb.war 8080
 cd ..
 chmod -R +777 GamaWeb
 cd GamaWeb
-java -jar tomcat_launcher.jar /offline_GamaWeb offline_GamaWeb.war 8080
+java -jar tomcat_launcher.jar /offline_GamaWeb offline_GamaWeb.war 8080 listening
 }
 
 
